328b7af958e510a156a22b0740e26c344c3ca573
ppanwar <ppanwar@cholachariots.com>
2024-02-24 18:33:11 +0530

    new update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -17,7 +17,7 @@ const HeroSection = () => {
            md:ml-9 font-bold mb-4">Welcome to VITPARK</h1>
           <p className="lg:text-2xl  mr-10
            lg:ml-12  text-center
-            text-xl font-serif md:ml-40  ml-16 md:text-xl mb-8">Your Gateway to Digital Innovation</p>
+            text-xl font-serif md:ml-30  ml-16 md:text-xl mb-8">Your Gateway to Digital Innovation</p>
           <p className="lg:text-1xl  font-serif lg:text-center md:text-xl
            lg:w-[590px] mb-8">At VITPARK, we are dedicated to driving digital transformation and empowering businesses to thrive in the digital age. With our comprehensive suite of services and expertise in digital infrastructure, we provide end-to-end support to help businesses unlock endless possibilities for success.</p>
         </div>
